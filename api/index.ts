1127fb014d34c2e60d45cff92dae1b83b64fefad
Jacob Waller <jacob.waller@bp.com>
2021-09-07 11:56:41 -0500

    put the script in the script...

--- api/index.ts
+++ api/index.ts
@@ -26,15 +26,15 @@ const chargingHandler = async (req: Express.Request, res: Express.Response) => {
           padding: 0;
         }
       </style>
-      let map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById("map"), {
-          center: { lat: -34.397, lng: 150.644 },
-          zoom: 8,
-        });
-      }
-      <script>
 
+      <script>
+        let map;
+        function initMap() {
+          map = new google.maps.Map(document.getElementById("map"), {
+            center: { lat: -34.397, lng: 150.644 },
+            zoom: 8,
+          });
+        }
       </script>
     </head>
     <body>
