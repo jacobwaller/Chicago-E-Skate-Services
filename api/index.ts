1c1676122e9a6947a4a6b75b3e616b5eb1dfd57f
Jacob Waller <jacob.waller@bp.com>
2021-09-07 12:25:08 -0500

    Try to fetch all marker info and display it

--- api/index.ts
+++ api/index.ts
@@ -5,15 +5,34 @@ import {
   getChargingSpots,
 } from './sheetController';
 import Express from 'express';
+import { getChargeSpots } from '../bot/utils/dbHandler';
+import { ChargeSpot } from '../bot/utils/types';
+
+const createMarkerText = (spot: ChargeSpot): string => {
+  return `
+    new google.maps.Marker({
+      position: {
+        lat: ${spot.lat}, 
+        lng: ${spot.lon}
+      },
+      map,
+      title: "${spot.description}",
+      label: "${spot.id}",
+      optimized: true,
+    });
+  `;
+};
 
 const chargingHandler = async (req: Express.Request, res: Express.Response) => {
   res.set('Access-Control-Allow-Origin', '*');
   res.set('Access-Control-Allow-Methods', 'GET');
+  const spots = await getChargeSpots();
+
   const ret = `
   <!DOCTYPE html>
   <html>
     <head>
-      <title>Simple Map</title>
+      <title>Chicago Charge Map</title>
       <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
       <style>
         #map {
@@ -31,16 +50,24 @@ const chargingHandler = async (req: Express.Request, res: Express.Response) => {
         let map;
         function initMap() {
           map = new google.maps.Map(document.getElementById("map"), {
-            center: { lat: -34.397, lng: 150.644 },
+            center: { lat: 41.8781, lng: -87.6298 },
             zoom: 8,
           });
+
+          ${spots
+            .map((spot) => {
+              createMarkerText(spot);
+            })
+            .join('\n')}
         }
       </script>
     </head>
     <body>
       <div id="map"></div>
       <script
-        src="https://maps.googleapis.com/maps/api/js?key=${process.env.MAPS_KEY}&callback=initMap&libraries=&v=weekly"
+        src="https://maps.googleapis.com/maps/api/js?key=${
+          process.env.MAPS_KEY
+        }&callback=initMap&libraries=&v=weekly"
         async
       ></script>
     </body>
